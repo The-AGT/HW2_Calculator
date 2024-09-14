d4465e8845f62a3eb84d79fb1e9cadea6fa3aa02
Максим Финк <maksimfink@MacBook-Pro-Maksim.local>
2024-09-14 18:22:35 +0300

    Реализован калькулятор с функциями в отдельных файлах

---
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "639434cf-fcf0-4b78-bbca-87605d12bd30",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def add(a, b):\n",
+    "    return a + b"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
